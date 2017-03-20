54fd12d1281614938b8035a31005b1608d4b7adb
AriSendic <arimsendic@gmail.com>
2017-03-20 03:51:58 -0500

    populated seed file with seed dump

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Text.create!([
+  {title: "try 8 with id", file: "Testing with raw JSON view!"},
+  {title: "try 9 with id", file: "Testing with raw JSON view!"},
+  {title: "try 10 with id", file: "Testing with raw JSON view!"},
+  {title: "try as hash", file: "Hi! I’m testing this text file."},
+  {title: "try 2 as hash", file: "Hi! I’m testing this text file."},
+  {title: "Media Math", file: "Evaluation will be based on application design, readability of the code and test coverage. Test coverage should demonstrate both success and failure conditions within reason. Included with the code should be some form of README on how to run the service and any dependencies not included."}
+])
